675fe8c5c05d1cc975d3da60b767cb0b63d98056
raymyat <33442901+raymyat@users.noreply.github.com>
2018-10-04 15:16:15 +0700

    ==

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule, 
+  MatFormFieldModule, 
+  MatInputModule, 
+  MatOptionModule, 
+  MatSelectModule, 
+  MatIconModule, 
+  MatButtonModule, 
+  MatCardModule, 
+  MatTableModule, 
+  MatDividerModule, 
+  MatSnackBarModule,
+  MatPaginatorModule } from '@angular/material';
 import { AppComponent } from './app.component';
-
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LandingComponent } from './landing/landing.component';
+const routes: Routes=[
+  {path: 'login', component: LogInComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'landing', component: LandingComponent},
+  {path: '', redirectTo: 'landing', pathMatch:'full'}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    SignUpComponent,
+    HomepageComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),//configure the router at routing level
+    MatToolbarModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatOptionModule, 
+    MatSelectModule,
+    MatIconModule, 
+    MatButtonModule, 
+    MatCardModule, 
+    MatTableModule, 
+    MatDividerModule, 
+    MatSnackBarModule,
+    MatPaginatorModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { MatToolbar, MatButton } from '@angular/material';
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
